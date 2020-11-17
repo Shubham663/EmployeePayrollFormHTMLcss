809a9dd78c872827761cb0e9dec8fc08a1b3bc35
unknown <mittalshubham05@gmail.com>
2020-11-17 12:39:42 +0530

    Add: populating from JSON using string literals

--- addEmployeePayroll.js
+++ addEmployeePayroll.js
@@ -3,21 +3,64 @@ window.addEventListener('DOMContentLoaded',(event) => {
 })
 
 const createInnerHtml= () => {
-    const headerHTML = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
-    const inHtml = `${headerHTML}
-        <tr>
-            <td><img class="profile" alt="" src="images/Ellipse -2.png"></td>
-            <td>Narayan Mahadevan</td>
-            <td>Male</td>
-            <td><div class="dept-label">HR</div>
-            <div class="dept-label">Finance</div></td>
-            <td>3000000</td>
-            <td>1 Nov 2020</td>
-            <td>
-                <img src="icons/delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
-                <img src="icons/create-black-18dp.svg" alt="edit" id="1" onclick="update(this)">
-            </td>
-        </tr>
-    `;
+    const headerHTML = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
+    const empPayrollList = createEmployeePayrollJSON();
+    let inHtml = `${headerHTML}`;
+    console.log(empPayrollList);
+    for(const empPayrollData of empPayrollList){
+        inHtml = `${inHtml}
+            <tr>
+                <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+                <td>${empPayrollData._name}</td>
+                <td>${empPayrollData._gender}</td>
+                <td>${getDeptHTML(empPayrollData._department)}</td>
+                <td>${empPayrollData._salary}</td>
+                <td>${empPayrollData._startDate}</td>
+                <td>
+                    <img src="icons/delete-black-18dp.svg" alt="delete" name="${empPayrollData._id}" onclick="remove(this)">
+                    <img src="icons/create-black-18dp.svg" alt="edit" name="${empPayrollData._id}" onclick="update(this)">
+                </td>
+            </tr>
+        `;
+    }
     document.getElementById('display').innerHTML= inHtml;
+}
+
+const getDeptHTML = (deptList) => {
+    let deptHTML = '';
+    for(const dept of deptList){
+        deptHTML = `${deptHTML} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHTML;
+};
+
+const createEmployeePayrollJSON = () => {
+    let employeePayrollListLocal = [
+        {
+            _name: 'Narayan Mahadevan',
+            _gender: 'male',
+            _department: [
+                'engineering',
+                'finance'
+            ],
+            _salary: '500000',
+            _startDate: '29 Oct 2019',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: 'images/Ellipse -2.png'
+        },
+        {
+            _name: 'Anarpa Shashantha Keerti Kumar',
+            _gender: 'female',
+            _department: [
+                'sales'
+            ],
+            _salary: '400000',
+            _startDate: '29 Oct 2019',
+            _note: '',
+            _id: new Date().getTime() + 1,
+            _profilePic: 'images/Ellipse -1.png'
+        }
+    ]
+    return employeePayrollListLocal;
 }
